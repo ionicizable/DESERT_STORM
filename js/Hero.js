d01c4d6bb9a31cdec90ad23f1c17c00ff17f1303
Ilya Shapiro <ilya.m.shapiro@gmail.com>
2019-11-14 19:31:38 +0300

    Created repository and pasted files

---
+++ js/Hero.js
@@ -0,0 +1,18 @@
+export default class Hero{
+	constructor(gameWidth,gameHeight){
+		var 
+		this.charwidth = 64;
+		this.charheight = 64;
+		this.srcPos = {
+			x: 0,
+			y: 0
+		}
+		this.destPos = {
+			x: 0,
+			y: 0
+		}
+	}
+	draw(ctx){
+		ctx.drawImage()
+	}
+}
